365500031a711386483522e6ab781b08897e7d14
HliasOuzounis <iliouzoun@outlook.com>
2022-11-30 20:03:00 +0200

    .

---
+++ src/add.rs
@@ -0,0 +1,7 @@
+use std::path::PathBuf;
+
+pub fn add(image_path: PathBuf, theme_name: Option<String>){
+    
+    return;
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,59 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "theme-picker", about = "Easy way to switch between multiple pywal themes")]
-enum ThemePicker {
-    /// <image-path> add new theme to selections
-    Add {
-        /// image path
-        #[structopt(parse(from_os_str))]
-        image_path: PathBuf,
+use add;
 
-        /// theme name (will default to image_name)
-        theme_name: Option<String>,
-    },
-    /// change theme to selection8uj
-    Select {
-        /// theme name
-        theme_name: Option<String>,
-    },
-    /// [theme name] remove theme from selections
-    Remove {
-        /// theme name
-        theme_name: Option<String>,
-    },
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "theme-picker",
+    author = "HliasOuzounis",
+    about = "Easy way to switch between multiple pywal themes"
+)]
+struct CLI {
+    #[structopt(subcommand)]
+    cmd: ThemePicker,
+}
+
+#[derive(Debug, StructOpt)]
+enum ThemePicker {
+    #[structopt(name = "add", about = "<image-path> add new theme to selections")]
+    Add(AddOptions),
+    #[structopt(name = "remove", about = "[theme name] remove theme from selections")]
+    Remove(RemoveOptions),
+    #[structopt(name = "select", about = "change theme to selection")]
+    Select(SelectOptions),
+}
+#[derive(Debug, StructOpt)]
+pub struct AddOptions {
+    /// image path
+    #[structopt(parse(from_os_str))]
+    image_path: PathBuf,
+
+    /// theme name (will default to image_name)
+    theme_name: Option<String>,
+}
+#[derive(Debug, StructOpt)]
+struct RemoveOptions {
+    /// theme name
+    theme_name: Option<String>,
+}
+#[derive(Debug, StructOpt)]
+struct SelectOptions {
+    /// theme name
+    theme_name: Option<String>,
 }
 
 fn main() {
-    let opt = ThemePicker::from_args();
-    println!("{:?}", opt);
+    let args = CLI::from_args();
+    match args.cmd {
+        ThemePicker::Add(opt) => {
+            add::add()
+        }
+        ThemePicker::Remove(opt) => {
+            println!("{:#?}", opt);
+        }
+        ThemePicker::Select(opt) => {
+            println!("{:#?}", opt);
+        }
+    }
 }
